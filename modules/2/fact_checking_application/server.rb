7097dfd1d9e7062f46d1d5ce3a8c0de582949624
Sam Morgan <s_morgan@me.com>
2017-03-15 13:29:17 +0000

    Add scaffold for module 3
    * Module 3 will start by transforming the existing application to use a framework
    * After this we'll implement sessions using the framework

--- modules/2/fact_checking_application/server.rb
+++ modules/2/fact_checking_application/server.rb
@@ -4,9 +4,12 @@ require 'net/http'
 
 post '/' do 
   parsed_request = JSON.parse(request.body.read)
-
+  p parsed_request
   number = parsed_request["request"]["intent"]["slots"]["Number"]["value"]
   fact_type = parsed_request["request"]["intent"]["slots"]["FactType"]["value"]
+
+  p "Heard number: #{number}"
+  p "Heard fact type: #{fact_type}"
 
   number_facts_uri = URI("http://numbersapi.com/#{ number }/#{ fact_type }")
   number_fact = Net::HTTP.get(number_facts_uri)
