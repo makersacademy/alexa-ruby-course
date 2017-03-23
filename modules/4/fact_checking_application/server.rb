02e9a92f87efd4c41e0c597513adc99bbad6db09
Sam Morgan <s_morgan@me.com>
2017-03-23 17:06:24 +0000

    Refactor Alexa Request to its own class, Alexa::Request
    * Used to extract Slot values
    * Used Extract Method to pull Number Fact things out to a method - TODO: refactor to a class

--- modules/4/fact_checking_application/server.rb
+++ modules/4/fact_checking_application/server.rb
@@ -1,15 +1,17 @@
 require 'sinatra'
+require './lib/alexa/request'
 require './lib/alexa/response'
 require 'net/http'
 
 post '/' do 
-  parsed_request = JSON.parse(request.body.read)
+  alexa_request = Alexa::Request.new(request)
 
-  number = parsed_request["request"]["intent"]["slots"]["Number"]["value"]
-  fact_type = parsed_request["request"]["intent"]["slots"]["FactType"]["value"]
-
-  number_facts_uri = URI("http://numbersapi.com/#{ number }/#{ fact_type }")
-  number_fact = Net::HTTP.get(number_facts_uri)
+  number_fact = fetch_number_fact(alexa_request.slot_value("Number"), alexa_request.slot_value("FactType"))
 
   Alexa::Response.new(number_fact).to_json
+end
+
+def fetch_number_fact(number, fact_type)
+  number_facts_uri = URI("http://numbersapi.com/#{ number }/#{ fact_type }")
+  Net::HTTP.get(number_facts_uri)
 end
