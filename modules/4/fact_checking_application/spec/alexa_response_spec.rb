bc303199bf8992e2c1f84b0e06febca95f9d1016
Sam Morgan <s_morgan@me.com>
2017-03-23 16:43:27 +0000

    Upgrade Alexa::Response to handle variable string inputs
    * Copied specs across from 'hello world' application
    * Extended to take an input, defaulting to "Hello World" if no input is provided

---
+++ modules/4/fact_checking_application/spec/alexa_response_spec.rb
@@ -0,0 +1,36 @@
+require 'alexa/response'
+
+RSpec.describe Alexa::Response do
+  subject(:response) { described_class.new }
+
+  describe '#to_json' do
+    it 'returns a JSON response with a custom string if provided' do
+      expected_response = {
+        version: "1.0",
+        response: {
+          outputSpeech: {
+              type: "PlainText",
+              text: "Custom String"
+            }
+        }
+      }.to_json
+
+      custom_response = described_class.new("Custom String").to_json
+      expect(custom_response).to eq expected_response
+    end
+
+    it 'returns a minimal JSON response otherwise' do
+      minimal_response = { 
+        version: "1.0",
+        response: {
+          outputSpeech: {
+              type: "PlainText",
+              text: "Hello World"
+            }
+        }
+      }.to_json
+      
+      expect(response.to_json).to eq minimal_response
+    end
+  end
+end
