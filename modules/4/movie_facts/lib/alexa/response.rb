35286add15f304c90158171bf419ba6a3bad57a8
Sam Morgan <s_morgan@me.com>
2017-03-24 14:19:38 +0000

    Use Extract Method on the Response to pull out the response construction to private methods

--- modules/4/movie_facts/lib/alexa/response.rb
+++ modules/4/movie_facts/lib/alexa/response.rb
@@ -3,19 +3,31 @@ require 'json'
 module Alexa
   class Response < Hash
     def initialize(output_text, session_attributes, end_session)
-      self[:version] = "1.0"
-      self[:sessionAttributes] = session_attributes unless session_attributes.empty?
-
-      self[:response] = Hash.new
-      self[:response][:outputSpeech] = Hash.new
-      self[:response][:outputSpeech][:type] = "PlainText"
-      self[:response][:outputSpeech][:text] = output_text
-
-      self[:response][:shouldEndSession] = end_session if end_session
+      set_version
+      set_session_attributes(session_attributes)
+      set_response(output_text, end_session)
     end
 
     def self.build(output_text = "Hello World", session_attributes = {}, end_session = false)
       new(output_text, session_attributes, end_session).to_json
+    end
+
+    private
+
+    def set_version
+      self[:version] = "1.0"
+    end
+
+    def set_session_attributes(session_attributes)
+      self[:sessionAttributes] = session_attributes unless session_attributes.empty?
+    end
+
+    def set_response(output_text, end_session)
+      self[:response] = Hash.new
+      self[:response][:outputSpeech] = Hash.new
+      self[:response][:outputSpeech][:type] = "PlainText"
+      self[:response][:outputSpeech][:text] = output_text
+      self[:response][:shouldEndSession] = end_session if end_session
     end
   end
 end
